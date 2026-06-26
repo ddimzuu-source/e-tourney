42fd4d33594fb275304a7f3f2a8ecda490df6e1c
drmwnmass <ddimzuu@gmail.com>
2026-06-26 16:56:25 +0700

    supabase storage dan modif judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Tourney Dashboard Admin</title>
+    <title>E-Tourney</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
