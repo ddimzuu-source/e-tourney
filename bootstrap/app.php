5d471c3ee4a634cbcb41f1e767a63e2ed8b2e25c
drmwnmass <ddimzuu@gmail.com>
2026-06-29 13:05:56 +0700

    middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+   ->withMiddleware(function (Middleware $middleware) {
+        // Daftarkan CORS kustom kamu di sini secara global
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
